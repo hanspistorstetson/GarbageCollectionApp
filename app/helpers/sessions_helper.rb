8894a7ba6836314bf9cf3e74b4185d7e26e21ee1
Hans Pistor <hpistor@stetson.edu>
2016-10-05 05:18:47 -0400

    Working basic login and logout forms

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,24 @@
+module SessionsHelper
+
+    #login the given user
+    def login(user)
+        session[:user_id] = user.id
+    end
+
+    # Logs out the current user.
+    def logout
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
+    # Returns the current logged-in user (if any).
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    # Returns true if the user is logged in, false otherwise.
+    def logged_in?
+      !current_user.nil?
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
 
     has_secure_password
 
+    # Returns the hash digest of the given string.
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Password.create(string, cost: cost)
+    end
 end
